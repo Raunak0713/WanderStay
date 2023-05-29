b699f618bb3c041d4048614a107bccd7600383ab
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-05-29 09:20:22 +0530

    api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
-app.get('/test', (req,res) => {
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+}));
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req, res) => {
     res.json("test ok");
-})
+});
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({ name, email, password });
+});
 
 app.listen(4000);
